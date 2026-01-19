8e10dc62c382f4d8aafbf843e3f0e327a8660c5a
Rakshith S Shetty <rakshithshetty@feserver.mirafra.com>
2026-01-19 14:12:57 +0530

    updated interface

--- src/ral_dma_interface.sv
+++ src/ral_dma_interface.sv
@@ -1,2 +1,16 @@
-interface (clk,rst);
+ interface ral_dma_inf(input logic clk,input logic rst_n);
+    logic wr_en;
+    logic rd_en;
+    logic [31:0] wdata;
+    logic [31:0] addr;
+    logic [31:0] rdata;
+        
+  clocking drv_cb@(posedge clk);
+    output wr_en,rd_en,wdata,addr;
+  endclocking
+  
+  clocking mon_cb@(posedge clk);
+    input rdata,wr_en,rd_en,wdata,addr;
+  endclocking
+
 endinterface
