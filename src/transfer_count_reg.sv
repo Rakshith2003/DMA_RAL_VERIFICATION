62b13e96568062b5c0231935290db03fa99463cd
Rakshith S Shetty <111422371+Rakshith2003@users.noreply.github.com>
2026-01-19 12:13:53 +0530

    Create transfer_count_reg.sv

---
+++ src/transfer_count_reg.sv
@@ -0,0 +1,16 @@
+class TRANSFER_COUNT extends uvm_reg;
+    `uvm_object_utils(TRANSFER_COUNT)
+    
+    rand uvm_reg_field transfer_count;
+   
+    function new(string name = "TRANSFER_COUNT");
+      super.new(name, 32, build_coverage(UVM_NO_COVERAGE));
+    endfunction
+   
+    virtual function void build();
+      transfer_count  = uvm_reg_field::type_id::create("transfer_count");
+      transfer_count.configure(this, 32, 0, "RO", 0, 32'h0, 1, 1, 1);
+      
+    endfunction 
+ 
+endclass
